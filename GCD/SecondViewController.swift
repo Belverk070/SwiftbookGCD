0d73f9023c8d996824a671ff7b54d28609bd9232
Belverk070 <vometlin@gmail.com>
2022-03-08 17:26:43 +0300

    Initial commit

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Василий Метлин on 08.03.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+    fileprivate func loginAlert() {
+        let alertController = UIAlertController(title: "Registered?", message: "Enter your login and password", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        alertController.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Enter login"
+        }
+        
+        alertController.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Enter password"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+    }
+}
---
+++ GCD/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Василий Метлин on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
